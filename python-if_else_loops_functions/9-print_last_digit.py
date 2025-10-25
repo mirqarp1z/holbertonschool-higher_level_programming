62660eb6a962f374a2f2e17676940246bef656c8
mirqarp1z <orucaydin13@gmail.com>
2025-10-25 19:02:02 +0000

    task

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+
+    if number > 0:
+        print(number % 10, end='')
+        return number % 10
+    elif number < 0:
+        print((number * (-1)) % 10, end='')
+        return (number * (-1)) % 10
+    else:
+        print("0", end='')
+        return 0
