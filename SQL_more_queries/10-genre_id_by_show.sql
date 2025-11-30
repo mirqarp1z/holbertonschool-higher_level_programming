4dd979cb6aaa2a3beaf40bb6f9f935b1d2c9811a
mirqarp1z <orucaydin13@gmail.com>
2025-11-30 19:25:31 +0000

    task

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,5 @@
--- Script that creates an user in MySQL server
--- Query to create the user 'user_0d_1' in MySQL server
--- Create user only if it does not already exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges on the entire MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- Apply changes
-FLUSH PRIVILEGES;
-
+-- Script that lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
